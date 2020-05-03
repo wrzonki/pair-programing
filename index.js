b86dd43f1ff7d56db42598db2532a0d70cbac5e6
Paweł Wrzosek <pawel.wrzosek@quarticon.com>
2020-05-03 21:52:29 +0200

    dodanie formularza

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const crawl = require('./crawl');
+const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'pug');
+app.use( bodyParser.json() );      
+app.use(bodyParser.urlencoded({     
+  extended: true
+})); 
 
 app.get('/', function (req, res) {
-    crawl.getData('http://pawel-wrzosek.pl').then((data) => {
+    res.render('index', {
+        title: 'Tytuł ',
+        message: 'Siema',
+    });
+});
+
+
+app.post('/result', (req, res) => {
+    console.log(req.body.title)
+    let url =  req.body.title
+    crawl.getData(url).then((data) => {
         res.render('index', {
             title: 'Tytuł ',
-            message: 'Siema',
+            message: 'Wynik',
             crawler: data,
         });
     });
-});
+  })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
